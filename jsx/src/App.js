1fcf8591d90e5f5acef2478fea57290fa55c30bf
Ana Pao Minchaca <42215143+anapao-minchaca@users.noreply.github.com>
2023-02-01 14:34:56 -0600

    projects from sections 1 to 3

---
+++ jsx/src/App.js
@@ -0,0 +1,7 @@
+// Make your component. Should be a function that returns JSX.
+function App() {
+    return <h1>My name is Ana!</h1>
+}
+
+// Export the component at the bottom of the line
+export default App;
